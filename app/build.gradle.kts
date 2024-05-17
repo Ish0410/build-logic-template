cddc0591de924586d44f6c6d87c6f3f7d141d279
임상현(파인원/미디어서비스사업본부) <140618231+Ish0410@users.noreply.github.com>
2024-05-17 16:25:59 +0900

    build-logic 프로젝트 생성
    build-logic 1차 Convention Plugin 적용
    - applicationConventionPlugin
    - applicationComposeConventionPlugin
    - featureConventionPlugin
    - libraryConventionPlugin
    - hiltConventionPlugin

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("imsh.android.application")
+    id("imsh.android.application.compose")
+    id("imsh.android.hilt")
+}
+
+android {
+    // TODO: namespace 프로젝트에 맞게 변경 필요
+    namespace = "com.example.build.sample"
+
+    defaultConfig {
+        // TODO: applicationId 프로젝트에 맞게 변경 필요
+        applicationId = "com.example.build.sample"
+
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "imsh.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "imsh.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "imsh.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature") {
+            id = "imsh.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "imsh.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+    }
+}
