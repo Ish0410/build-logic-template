d36b04fe2679286102a4797eca4b4ece8f0c66b7
임상현(파인원/미디어서비스사업본부) <140618231+Ish0410@users.noreply.github.com>
2024-05-24 15:57:29 +0900

    gradle 설정 변경
    - module import
    - build 오류 exclude 처리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:designsystem"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.hilt.navigation.compose)
